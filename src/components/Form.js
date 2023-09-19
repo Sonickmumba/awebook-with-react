1c87e240f3352866fdc4636c43ea43de2d041298
Mumba <27748812@sun.ac.za>
2023-09-19 22:41:26 +0200

    Update form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,37 @@
-import React from "react";
+import React from 'react';
 
 const Forms = () => {
   const a = 'sonick';
   return (
-    <div className="form-container"> {/* Apply CSS class */}
-        <h2>Author and Book Title Form</h2>
-        <form onSubmit={this.handleSubmit}>
-            <div className="form-group"> {/* Apply CSS class */}
-                <label htmlFor="authorName">Author's Name:</label>
-                <input
-                    type="text"
-                    id="authorName"
-                    name="authorName"
-                    value={""}
-                    onChange={""}
-                    required
-                />
-            </div>
-            <div className="form-group"> {/* Apply CSS class */}
-                <label htmlFor="bookTitle">Book Title:</label>
-                <input
-                    type="text"
-                    id="bookTitle"
-                    name="bookTitle"
-                    value={this.state.bookTitle}
-                    onChange={this.handleInputChange}
-                    required
-                />
-            </div>
-            <div className="form-group"> {/* Apply CSS class */}
-                <button type="submit">Submit</button>
-            </div>
-        </form>
+    <div className="form-container">
+      <h2>
+        { a }
+      </h2>
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            id="authorName"
+            name="authorName"
+            value=""
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="bookTitle"
+            name="bookTitle"
+            value=""
+            required
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 };
-
 
 export default Forms;
