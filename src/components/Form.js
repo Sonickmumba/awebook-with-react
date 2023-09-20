4d2bb7729ce6d4eab8a64fd379bde5ddbecbd84f
Mumba <27748812@sun.ac.za>
2023-09-20 15:30:34 +0200

    Ahandle change event handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Forms = () => {
   const a = 'sonick';
+  const [authorData, setAuthorData] = useState({
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setAuthorData({
+      ...authorData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    // const storedAuthorData = JSON.parse(localStorage.getItem('AuthorsBooks')) || [];
+    // storedAuthorData.push(authorData);
+    // localStorage.setItem('AuthorsBooks', JSON.stringify(storedAuthorData));
+  };
   return (
     <div className="form-container">
       <h2>
         { a }
       </h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <input
             type="text"
             id="authorName"
             name="authorName"
             required
+            placeholder="Author's name"
+            onChange={handleInputChange}
           />
         </div>
         <div className="form-group">
@@ -22,6 +40,8 @@ const Forms = () => {
             id="bookTitle"
             name="bookTitle"
             required
+            placeholder="Book Title"
+            onChange={handleInputChange}
           />
         </div>
         <div className="form-group">
