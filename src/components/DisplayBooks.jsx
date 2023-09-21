12b4b78dc5475443c24ea81a31cdd9931986add5
Sonick Mumba <kingellie.mumba@gmail.com>
2023-09-21 21:49:10 +0200

    Add html for books

--- src/components/DisplayBooks.jsx
+++ src/components/DisplayBooks.jsx
@@ -3,8 +3,13 @@ import React, { useState } from "react";
 const display = () => {
   const a = "sonick"
   const [book, setBook] = useState({});
+
   return (
-    <div>{ a }</div>
+    <div>
+      <h2>Book Display</h2>
+      <p>{ a }</p>
+      <div id="books-div"></div>
+    </div>
   )
 };
 
