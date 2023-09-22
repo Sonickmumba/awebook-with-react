3fb2a15943c0c0cb21883afd6fc900b0da7b0f1b
Sonick Mumba <kingellie.mumba@gmail.com>
2023-09-22 11:34:24 +0200

    Remove unwanted code

--- src/components/DisplayBooks.jsx
+++ src/components/DisplayBooks.jsx
@@ -4,11 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 const Display = () => {
   const [book, setBook] = useState([]);
   const uniqueId = uuidv4();
-  const storedData = localStorage.getItem('AuthorBooks');
-  console.log(storedData);
   useEffect(() => {
     const storedData = localStorage.getItem('AuthorsBooks');
-    console.log(storedData);
 
     if (storedData) {
       const parsedData = JSON.parse(storedData);
